c240114c566d5bc2953f2377653403f93ddc4f8d
Wang Yanqing <me@oreki.moe>
2016-12-21 11:17:46 +0800

    Init repo

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+entry_points = [
+]
+
+requires = [
+]
+
+setup(
+    name='makiki',
+    version='0.0.1',
+    description='',
+    long_description='',
+    author='Wang Yanqing',
+    author_email='me@oreki.moe',
+    packages=find_packages(),
+    url='http://github.com/faith0811/makiki',
+    include_package_data=True,
+    entry_points={
+        'console_scripts': entry_points,
+    },
+    zip_safe=False,
+    install_requires=requires,
+)
