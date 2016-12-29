9d9dd7f83c143113bc6feaa960f91edcfcf9afa1
Wang Yanqing <me@oreki.moe>
2016-12-29 14:17:57 +0800

    Expand schema

--- makiki/schema.py
+++ makiki/schema.py
@@ -1,5 +1,18 @@
 # -*- coding: utf-8 -*-
 
+import collections
 
-class BasicSchema(object):
-    pass
+
+Type = collections.namedtuple('Type', ['name', '_type', 'examples'])
+
+
+class SchemaMeta(type):
+    def __init__(self, name, parents, attrs):
+        super().__init__(name, parents, attrs)
+        assert isinstance(attrs['fields'])
+        for field in attrs['fields']:
+            assert isinstance(field, Type)
+
+
+class BasicSchema(object, metaclass=SchemaMeta):
+    fields = []
