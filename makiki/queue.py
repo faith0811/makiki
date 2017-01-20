a9e5732c1ae8c347cf9f5067acaa1a9bffe81ee1
Wang Yanqing <me@oreki.moe>
2017-01-20 15:30:05 +0800

    Fix functools partial has no func name, and bump version to 0.1.14

--- makiki/queue.py
+++ makiki/queue.py
@@ -102,7 +102,7 @@ def send_task(module_name, api_name, *args, countdown=0, async_api=None, apply_q
 
 
 def make_async_task(function_executor, retry_wait=5):
-    return functools.partial(async_task, function_executor=function_executor, retry_wait=retry_wait)
+    return functools.wraps(async_task)(functools.partial(async_task, function_executor=function_executor, retry_wait=retry_wait))
 
 
 def async_task(self, module_name, api_name, retry_wait=5, func_executor=None, *args, **kwargs):
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requires = [
 
 setup(
     name='makiki',
-    version='0.1.13',
+    version='0.1.14',
     description='Web service utils and generator.',
     long_description='',
     author='Wang Yanqing',
