d78220a73c22234e5a2b0d1dca47211b90dc4191
Wang Yanqing <me@oreki.moe>
2017-04-05 12:37:10 +0800

    Bump version to 0.2.7

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requires = [
 
 setup(
     name='makiki',
-    version='0.2.6',
+    version='0.2.7',
     description='Web service utils and generator.',
     long_description='',
     author='Wang Yanqing',
