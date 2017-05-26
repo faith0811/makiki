fdda5078339718159d20b8de56dd4f3d36061368
Wang Yanqing <me@oreki.moe>
2017-05-26 14:40:29 +0800

    Bump version to 0.2.14

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='makiki',
-    version='0.2.13',
+    version='0.2.14',
     description='Web service utils and generator.',
     long_description=readme,
     license='MIT',
