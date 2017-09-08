d29741c9fb28c89438c1adf06d8830bb28bb251f
Wang Yanqing <me@oreki.moe>
2017-09-08 14:53:11 +0800

    Support passing retry wait in kwargs

--- makiki/queue.py
+++ makiki/queue.py
@@ -95,6 +95,9 @@ def send_task(module_name, api_name, *args, countdown=0, async_api=None, apply_q
 
 def make_async_task(function_executor, retry_wait=5):
     def _f(self, module_name, api_name, *args, **kwargs):
+        if 'retry_wait' in kwargs:
+            retry_wait = kwargs['retry_wait']
+            del kwargs['retry_wait']
         return async_task(self, module_name, api_name, retry_wait, function_executor, *args, **kwargs)
     return _f
 
